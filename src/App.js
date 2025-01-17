7b681972686b387acf66a350eb1f8c6740e4db9a
Michael Sturgeon <1993msturgeon@gmail.com>
2025-01-17 14:32:47 +0000

    Install react router, and wire up navbar links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import { Container } from 'react-bootstrap';
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home Page</h1>} />        
+          <Route exact path='/login' render={() => <h1>Login</h1>} />
+          <Route exact path='/register' render={() => <h1>Register</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import styles from '../styles/NavBar.module.css';
 import { Navbar, Container, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed='top'>
         <Container>
-            <Navbar.Brand>Hunter<i class="fa-brands fa-raspberry-pi fa-xl"></i>Gatherer</Navbar.Brand>
+            <NavLink to='/'>
+                <Navbar.Brand>Hunter<i class="fa-brands fa-raspberry-pi fa-xl"></i>Gatherer</Navbar.Brand>
+            </NavLink>            
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                    <Nav.Link >Home</Nav.Link>
-                    <Nav.Link >Login</Nav.Link>                    
-                    <Nav.Link >Register</Nav.Link>                    
+                    <NavLink to='/'>Home</NavLink>
+                    <NavLink to='/login'>Login</NavLink>                    
+                    <NavLink to='/register'>Register</NavLink>                    
                     </Nav>                        
             </Navbar.Collapse>
         </Container>        
