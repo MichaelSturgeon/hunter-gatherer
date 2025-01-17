0f94bc56f5f8023b8a618acd2ab79a280af55ea3
Michael Sturgeon <1993msturgeon@gmail.com>
2025-01-17 16:24:00 +0000

    Add hover effect & active-link styling to navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,15 +7,15 @@ const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed='top'>
         <Container>
-            <NavLink to='/'>
-                <Navbar.Brand>Hunter<i class="fa-brands fa-raspberry-pi fa-xl"></i>Gatherer</Navbar.Brand>
+            <NavLink exact className={styles.NavLink} to='/'>
+                <Navbar.Brand>Hunter<i class="fa-brands fa-raspberry-pi"></i>Gatherer</Navbar.Brand>
             </NavLink>            
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/login'>Login</NavLink>                    
-                    <NavLink to='/register'>Register</NavLink>                    
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to='/'>Home</NavLink>
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to='/login'>Login</NavLink>                    
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to='/register'>Register</NavLink>                    
                     </Nav>                        
             </Navbar.Collapse>
         </Container>        
