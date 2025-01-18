75f65bf1897e8d8d1997c71fcd503ed0165129e2
Michael Sturgeon <1993msturgeon@gmail.com>
2025-01-18 14:27:26 +0000

    Add sign-in functionality to sign-in form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,22 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory} from "react-router-dom";
 import Styles from "../../App.module.css";
 
 import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import axios from "axios";
 
 const SignInForm = () => {
+    const [signInData, setSignInData] = useState({
+      username: "",
+      password: "",
+    });
+
+    const {username, password} = signInData
+    const history = useHistory();
+    const handleChange = (event) => {
+      setSignInData({
+        ...signInData,
+        [event.target.name]: event.target.value,
+      });
+    };
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        await axios.post('/dj-rest-auth/login/', signInData);
+        history.push('/');        
+      } catch (error) {
+        
+      }
+    }
+
   return (
     <Row >
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${Styles.Content} p-4`}>
           <h1 >Login</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="username">
                     <Form.Label className="d-none">Username</Form.Label>
                     <Form.Control 
                     type="text" 
                     placeholder="Username"
-                    name="username" />                    
+                    name="username"
+                    value={username}
+                    onChange={handleChange}/>                    
                 </Form.Group>                
 
                 <Form.Group controlId="password">
@@ -24,11 +51,13 @@ const SignInForm = () => {
                     <Form.Control 
                     type="password" 
                     placeholder="Password" 
-                    name="password"/>
+                    name="password"
+                    value={password}
+                    onChange={handleChange}/>
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
-                    Register
+                    Login
                 </Button>
             </Form>
             <Link  to="/register">
