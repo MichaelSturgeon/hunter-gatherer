a79c3868087bd03c0f629659ad506877012f72a5
Michael Sturgeon <1993msturgeon@gmail.com>
2025-01-19 19:55:47 +0000

    Create axios interceptors (axiosRes & axiosReq) to refresh access tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://hunter-gatherer-api-42e8d7d609ef.herokuapp.com/"
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
