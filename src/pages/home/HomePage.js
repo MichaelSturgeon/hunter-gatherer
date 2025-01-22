97f8435c67192ae02ccd3aa0e514b9acbc54f712
Michael Sturgeon <1993msturgeon@gmail.com>
2025-01-22 18:28:55 +0000

    Add further styling & jumbotron image to home page

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Jumbotron, Row } from 'react-bootstrap';
 import appStyles from '../../App.module.css'
 import homePageStyle from '../../styles/HomePage.module.css'
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <Row >
-      <Col className="my-auto py-2 p-sm-2" md={12}>        
-        <Container className={`${appStyles.Content} p-4 mb-2`}>
-          <h1>Compare, Decide, Save - The Ultimate Product Comparison Tool</h1>
-          <p>Find the best deals and make smarter buying decisions with our comprehensive product comparisons. Select a category to begin!</p>           
-        </Container>        
-        <Container className={`${appStyles.Content} p-4`}>
-          <Button className={homePageStyle.Button} variant="link"><i class="fa-solid fa-dice"></i><span>Board Games</span></Button>            
-          <Button className={homePageStyle.Button} variant="link"><i class="fa-regular fa-square-plus"></i><span>More</span></Button>                      
+      <Col className="my-auto py-2 p-md-2">
+        <Jumbotron className={homePageStyle.Jumbotron}>
+          <Container className={`${homePageStyle.overlay} p-3`}>
+            <h1 className={homePageStyle.Header}>Compare, Decide, Save<span className='d-none d-sm-block'> - The Ultimate Product Comparison Tool!</span></h1>
+            <p>Find the best deals and make smarter buying decisions with our comprehensive product comparisons. Select a category below to begin!</p>           
+          </Container>
+        </Jumbotron>        
+                
+        <Container className={`${appStyles.Content} mt-2`}>
+          <Link >
+          <Button className={homePageStyle.Button} variant="link"><i class="fa-solid fa-dice"></i><span>Board Games</span></Button>
+          </Link>
+          <Link >
+          <Button className={homePageStyle.Button} variant="link"><i class="fa-regular fa-square-plus"></i><span>More</span></Button>
+          </Link>                     
         </Container>    
       </Col>
     </Row>
